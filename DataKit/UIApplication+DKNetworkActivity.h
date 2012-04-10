4289d444227f6fa5d0acaf6c5debdc1e7b344975
Erik Aigner <erik@chocomoko.com>
2012-04-10 11:24:08 +0200

    moved network activity category out of private folder

--- DataKit/UIApplication+DKNetworkActivity.h
+++ DataKit/UIApplication+DKNetworkActivity.h
@@ -8,10 +8,26 @@
 
 #import <UIKit/UIKit.h>
 
+
+/**
+ Helper category for displaying the network activity indicator
+ */
 @interface UIApplication (DKNetworkActivity)
 
+/**
+ Begin a network activity
+ */
 + (void)beginNetworkActivity;
+
+/**
+ End a network activity. Must be balanced with -beginNetworkActivity.
+ */
 + (void)endNetworkActivity;
+
+/**
+ Returns the number of current network activities
+ @return The number of current network activities
+ */
 + (NSInteger)networkActivityCount;
 
 @end
